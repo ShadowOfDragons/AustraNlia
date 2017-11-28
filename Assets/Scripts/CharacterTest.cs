647da69ab6e7829e6df901217baa0a41e9b52d27
Zoe Gonzalez <zgonreq@gmail.com>
2017-11-28 19:06:59 +0100

    Added Character Follow

---
+++ Assets/Scripts/CharacterTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTest : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject point;
+    private CharacterTest player;
+    private Vector3 target;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<CharacterTest>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        target = point.transform.position;
+        if(this.gameObject.tag == "Player")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+        else if(this.gameObject.tag == "Follower")
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform.position;
+            target.x = target.x + 2;
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+
+	}
+
+    public void CharacterChange()
+    {
+        if(this.gameObject.tag == "Player")
+        {
+            this.gameObject.tag = "Follower";
+            player.enabled = !player.enabled;
+        }
+        else if (this.gameObject.tag == "Follower")
+        {
+            this.gameObject.tag = "Player";
+            player.enabled = !player.enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public CharacterTest player;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+//        if(Input.GetKeyDown(KeyCode.LeftShift)) change.Update();
+	}
+}
