3f5271450cc658c6f9ddeb55f6aba868e2487196
LauraCoboRuiz <lcoboruiz@gmail.com>
2017-12-05 19:02:31 +0100

    Update Menu
    Add some buttons that changes the resolution of the screen and complete some code

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-
 	public void QuitGame()
     {
         Application.Quit();
@@ -14,5 +13,39 @@ public class MenuButtons : MonoBehaviour
     public void LoadScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex);
+    }
+
+    //RESOLUTION
+    public void X1080()
+    {
+        Debug.Log("Done");
+        Screen.SetResolution(1920, 1080, false);
+    }
+    public void X720()
+    {
+        Screen.SetResolution(1280, 720, false);
+    }
+    public void X1200()
+    {
+        Screen.SetResolution(1600, 1200, false);
+    }
+    public void FullScreen()
+    {
+        if(Screen.fullScreen == true)
+        {
+            Screen.fullScreen = false;
+            Debug.Log("Fulled");
+        }
+        else if(Screen.fullScreen == false)
+        {
+            Screen.fullScreen = true;
+            Debug.Log("NotFulled");
+        }
+    }
+
+    //GRAPHIC QUALITY
+    public void Ultra()
+    {
+        
     }
 }
