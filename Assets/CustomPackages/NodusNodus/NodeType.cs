6fbf7d43704d3620b09613d5788e4518812b822b
Zoé González Requejo <zgonreq@gmail.com>
2017-12-08 13:03:23 +0100

    Cleaned Up Project + Deleted Nodus ndus

--- Assets/CustomPackages/NodusNodus/NodeType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NodeType
-{
-    public enum Type { Path, Player, Enemy, Hazard, undefined}
-    public Type type;
-}
